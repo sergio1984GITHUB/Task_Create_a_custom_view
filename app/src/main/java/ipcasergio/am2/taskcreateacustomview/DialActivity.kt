309c6237ff4aabf9b093b02716c14d691ae5b75c
Sérgio <a18074@alunos.ipca.pt>
2020-12-14 02:27:13 +0000

    Initial commit Completo!

---
+++ app/src/main/java/ipcasergio/am2/taskcreateacustomview/DialActivity.kt
@@ -0,0 +1,153 @@
+package ipcasergio.am2.taskcreateacustomview
+
+
+// import android.view.accessibility.AccessibilityNodeInfo
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import android.view.accessibility.AccessibilityNodeInfo
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
+
+
+   private enum class FanSpeed(val label: Int) {
+           OFF(R.string.fan_off),
+           LOW(R.string.fan_low),
+           MEDIUM(R.string.fan_medium),
+           HIGH(R.string.fan_high);
+
+       fun next() = when ( this){
+           OFF -> LOW
+           LOW -> MEDIUM
+           MEDIUM -> HIGH
+           HIGH -> OFF
+       }
+   }
+
+    private const val RADIUS_OFFSET_LABEL = 30
+    private const val RADIUS_OFFSET_INDICATOR = -35
+
+class DialView @JvmOverloads constructor(
+
+    context: Context,
+    attrs: AttributeSet? = null,
+    defSaleAttr : Int = 0
+) : View( context, attrs, defSaleAttr) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0F
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
+        private var radius = 0.0F
+        private var fanSpeed = FanSpeed.OFF
+
+        private val pointPosition: PointF = PointF(0.0F, 0.0F)
+
+
+          private var fanSpeedLowColor : Int
+          private var fanSpeedMediumColor : Int
+          private var fanSpeedMaxColor : Int
+
+         init {
+             isClickable = true
+
+             val typedArray = context.obtainStyledAttributes(attrs,R.styleable.DialView)
+             fanSpeedLowColor=typedArray.getColor(R.styleable.DialView_fanColor1,0)
+             fanSpeedMediumColor = typedArray.getColor(R.styleable.DialView_fanColor2,0)
+             fanSpeedMaxColor = typedArray.getColor(R.styleable.DialView_fanColor3,0)
+             typedArray.recycle()
+
+             updateContentDescription()
+
+             ViewCompat.setAccessibilityDelegate(this, object : AccessibilityDelegateCompat() {
+                 override fun onInitializeAccessibilityNodeInfo(host: View, info: AccessibilityNodeInfoCompat) {
+                         super.onInitializeAccessibilityNodeInfo(host, info)
+                         val customClick =
+                             AccessibilityNodeInfoCompat.AccessibilityActionCompat(
+                                 AccessibilityNodeInfo.ACTION_CLICK,
+                                 context.getString(
+                                     if (fanSpeed != FanSpeed.HIGH)
+                                         R.string.change else R.string.reset)
+                             )
+                         info?.addAction(customClick)
+                     }
+                 })
+             }
+
+
+    override fun performClick(): Boolean {
+        if (super.performClick())  return true
+
+           fanSpeed = fanSpeed.next()
+
+            updateContentDescription()
+        invalidate()
+        return true
+    }
+
+        override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeiht: Int) {
+
+            radius = (min(width, height) / 2.0 * 0.8).toFloat()
+        }
+
+
+
+        override fun onDraw(canvas: Canvas?) {
+            super.onDraw(canvas)
+
+             paint.color = when (fanSpeed){
+                 FanSpeed.OFF -> Color.GRAY
+                 FanSpeed.LOW -> fanSpeedLowColor
+                 FanSpeed.MEDIUM -> fanSpeedMediumColor
+                 FanSpeed.HIGH -> fanSpeedMaxColor
+             }
+
+
+
+            // Set dial background color to green if selection not off.
+            //paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+
+            // Draw the dial
+            canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+
+            // Draw the indicator circle
+            val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+            pointPosition.computerXYForSpeed(fanSpeed, markerRadius)
+            paint.color = Color.BLACK
+            canvas?.drawCircle(pointPosition.x, pointPosition.y, radius/12, paint)
+
+            // Draw the text labels.
+            val labelRadius = radius + RADIUS_OFFSET_LABEL
+            for (i in FanSpeed.values()) {
+                pointPosition.computerXYForSpeed (i, labelRadius)
+                val label = resources.getString(i.label)
+                canvas?.drawText(label, pointPosition.x, pointPosition.y, paint)
+            }
+
+       }
+            private fun PointF.computerXYForSpeed(pos: FanSpeed, radius: Float) {
+            //Angles are in radians.
+                val startAngle = Math.PI * (9 / 8.0)
+                val angle = startAngle + pos.ordinal * (Math.PI / 4)
+                x = (radius * cos(angle)).toFloat() + width / 2
+                y = (radius * sin(angle)).toFloat() + height / 2
+    }
+
+        private fun updateContentDescription(){
+            contentDescription = resources.getString(fanSpeed.label)
+
+
+        }
+
+
+  }
+
+
+
---
+++ app/src/main/java/ipcasergio/am2/taskcreateacustomview/MainActivity.kt
@@ -0,0 +1,22 @@
+
+
+package ipcasergio.am2.taskcreateacustomview
+
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+}
+
+
